6465aab2baf357bddea1c517d5f6998a1a0ce9ef
nhajouji <115039038+nhajouji@users.noreply.github.com>
2026-06-13 10:32:52 -0400

    Fixed negative trace bug

--- pycode/ecqf_tools.py
+++ pycode/ecqf_tools.py
@@ -45,7 +45,8 @@ def ap_in_pc_data(ap:tuple[int,int])->bool:
     if ap[0]==0:
         return ap[1] in get_ssps_pc()
     else:
-        return ap in get_aps_pc()
+        a,p = ap
+        return (abs(a),p) in get_aps_pc()
 
 # Misc. tools
 def abc_to_tau(abc:tuple[int,int,int])->np.array:
